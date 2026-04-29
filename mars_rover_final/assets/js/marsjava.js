8ff9146c612b4be955a6fb20879878c7ecd759fd
jhicks3-ualr <jhicks3@ualr.edu>
2026-04-29 06:22:36 +0000

    working on final validations and css cleanup

---
+++ mars_rover_final/assets/js/marsjava.js
@@ -0,0 +1,24 @@
+const toggleSwitch = document.querySelector('.theme-slider input[type="checkbox"]');
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
